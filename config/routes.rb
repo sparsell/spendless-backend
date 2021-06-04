e25385bb1135ecca6cd740fda98d09198c998a39
Sonja Parsell <hello@sparsell.com>
2021-06-04 08:41:34 -0400

    added goals routes/resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+      namespace :v1 do
+          resources :goals, only: [:index]
+        #   resources :totals, only: [:create, :edit, :update]
+        #   resources :spendless_amounts, only: [:create]
+      end
+  end
 end
