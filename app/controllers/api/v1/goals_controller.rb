98ab9463c7ecdc212f8efff8f2f6e956385b3e76
Sonja Parsell <hello@sparsell.com>
2021-06-08 10:50:57 -0400

    added create action and strong params - goals

--- app/controllers/api/v1/goals_controller.rb
+++ app/controllers/api/v1/goals_controller.rb
@@ -1,7 +1,23 @@
 class Api::V1::GoalsController < ApplicationController
 
     def index
-        @goals = Goal.all 
+        goals = Goal.all 
+        render json: goals
     end
+
+    def create
+        goal = Goal.create(goal_params)
+        if goal.save
+            render json
+        else
+
+        end
+    end
+
+    private
+
+        def goal_params
+            params.require(goal).permit(:goal_amount, :description)
+        end
 
 end
