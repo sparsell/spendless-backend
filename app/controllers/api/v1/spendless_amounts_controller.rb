7c3d81708a9364165ee9a7bef795a3d0b60a66ca
Sonja Parsell <hello@sparsell.com>
2021-07-21 17:59:17 -0400

    added create action and strong params

--- app/controllers/api/v1/spendless_amounts_controller.rb
+++ app/controllers/api/v1/spendless_amounts_controller.rb
@@ -5,4 +5,21 @@ class Api::V1::SpendlessAmountsController < ApplicationController
         render json: SpendlessAmountSerializer.new(spendless_amounts)
     end
 
+    def create
+        spendless_amount.new(spendless_amount_params)
+        if spendless_amount.save    
+            render jeson: spendless_amount, status: :accepted
+        else
+            render json: {errors: spendless_amount.errors.full_messages }, status: :unprocessible_entity
+        end
+    end
+
+
+    private
+
+    def spendless_amount_params
+        params.require(:spendless_amount).permit(:description, :total_id)
+    end
+
+
 end
