f8b8f7126fd422dea3a52476b9db03308364d282
Sonja Parsell <hello@sparsell.com>
2021-07-26 16:18:18 -0400

    missing ","

--- app/serializers/spendless_amount_serializer.rb
+++ app/serializers/spendless_amount_serializer.rb
@@ -1,5 +1,5 @@
 class SpendlessAmountSerializer
   include JSONAPI::Serializer
-  attributes :amount, :description :total_id, :total
+  attributes :amount, :description, :total_id, :total
   belongs_to :total
 end
