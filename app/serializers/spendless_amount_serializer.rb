e93012f056aebd883b5998a514fe87a704f98539
Sonja Parsell <hello@sparsell.com>
2021-07-29 06:42:02 -0400

    removed unnecessary attributes

--- app/serializers/spendless_amount_serializer.rb
+++ app/serializers/spendless_amount_serializer.rb
@@ -1,5 +1,6 @@
 class SpendlessAmountSerializer
   include JSONAPI::Serializer
-  attributes :amount, :description, :total_id, :total
+#   attributes :amount, :description, :total_id
+  attributes :amount, :description
   belongs_to :total
 end
