4eb2e601a0b622cf643dabc9d0332db3febdb1b2
sparsell <hello@sparsell.com>
2021-08-19 11:42:56 -0400

    added created_at to serializer attributes

--- app/serializers/spendless_amount_serializer.rb
+++ app/serializers/spendless_amount_serializer.rb
@@ -1,7 +1,7 @@
 class SpendlessAmountSerializer
   include JSONAPI::Serializer
 #   attributes :amount, :description, :total_id
-  attributes :amount, :description
+  attributes :amount, :description, :total_id, :created_at
   belongs_to :total
 
 end
